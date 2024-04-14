e7635cc8bb7a4d2d109c9bf66448dd39bb61b4b0
Ofek Ellanbogen <m4u_ofek@menahel4u.com>
2024-04-14 17:24:13 +0300

    updating some things

--- install.sh
+++ install.sh
@@ -3,25 +3,26 @@
 # Replace these paths with the actual paths to your files
 EXECUTABLE_PATH="/home/ofek/dist/cdlog"
 CONFIG_FILE_PATH="/home/ofek/dist/cdlog.conf"
-SERVICE_UNIT_FILE_PATH="/etc/systemd/system/cdlog.service"
+SERVICE_UNIT_FILE_PATH="/home/ofek/cdlog.service"
 
 # Copy executable to /usr/local/bin
-sudo cp "$EXECUTABLE_PATH" /usr/local/bin/cdlog
+sudo cp $EXECUTABLE_PATH /usr/local/bin/cdlog
 
-#create directory
+#recreating the directory
+sudo rm -rf /etc/cdlog
 sudo mkdir /etc/cdlog
 
 # Copy configuration file to /etc/cdlog
-sudo cp "$CONFIG_FILE_PATH" /etc/cdlog/cdlog.conf
+sudo cp $CONFIG_FILE_PATH /etc/cdlog/cdlog.conf
 
 # Copy service unit file to /etc/systemd/system
-sudo cp "$SERVICE_UNIT_FILE_PATH" /etc/systemd/system/cdlog.service
+sudo cp $SERVICE_UNIT_FILE_PATH /etc/systemd/system/cdlog.service
 
 # Reload systemd to pick up new service unit file
 sudo systemctl daemon-reload
 
 # Enable and start the service
-sudo systemctl enable your_service
-sudo systemctl start your_service
+sudo systemctl enable cdlog.service
+sudo systemctl start cdlog.service
 
 echo "Service installed successfully."
