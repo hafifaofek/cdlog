a4fe38a4609fe5a4638675bffe7490aa9f4fa082
Ofek Ellanbogen <m4u_ofek@menahel4u.com>
2024-04-14 18:34:15 +0300

    updating some things

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Replace these paths with the actual paths to your files
-EXECUTABLE_PATH="/home/ofek/dist/cdlog"
-CONFIG_FILE_PATH="/home/ofek/dist/cdlog.conf"
-SERVICE_UNIT_FILE_PATH="/home/ofek/cdlog.service"
-
-# Copy executable to /usr/local/bin
-sudo cp $EXECUTABLE_PATH /usr/local/bin/cdlog
+EXECUTABLE_PATH="./cdlog"
+CONFIG_FILE_PATH="./cdlog.conf"
+SERVICE_UNIT_FILE_PATH="./cdlog.service"
 
 #recreating the directory
 sudo rm -rf /etc/cdlog
 sudo mkdir /etc/cdlog
+
+# Copy executable to /usr/local/bin
+sudo cp $EXECUTABLE_PATH /usr/local/bin/cdlog
+sudo cp $EXECUTABLE_PATH /etc/cdlog/cdlog
 
 # Copy configuration file to /etc/cdlog
 sudo cp $CONFIG_FILE_PATH /etc/cdlog/cdlog.conf
