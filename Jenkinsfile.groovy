8c10439407d913911a63f4f0bb46f97c331221f5
Ofek Ellanbogen <m4u_ofek@menahel4u.com>
2024-04-15 18:14:34 +0300

    with ofek pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent {label 'linux'}
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Get some code from a GitHub repository
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: '81abb510-4bb6-471e-91a3-9495e63b542a', url: 'https://github.com/hafifaofek/cdlog.git']])
+
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh '/home/ofek/.local/bin/pyinstaller --onefile cdlog.py'
+                sh 'sudo mv cdlog.conf dist/'
+                sh 'sudo mkdir cdlog_package'
+                sh 'sudo mv cdlog.service cdlog_package/'
+                sh 'sudo mv dist/* cdlog_package/'
+                sh 'sudo mv install.sh cdlog_package/'
+                sh 'sudo mv README.md cdlog_package/'
+                sh 'sudo chmod +x cdlog_package/install.sh'
+                sh 'tar -czvf cdlog_install_package.tar.gz cdlog_package'
+                sh 'sudo mv -f cdlog_install_package.tar.gz /home/ofek'
+                sh 'sudo mv versions_site.html /data/index.html'
+                sh 'sudo mv -f nginx.conf /etc/nginx/nginx.conf'
+                sh 'sudo systemctl restart nginx'
+            }
+        }
+        stage('Clean') {
+            steps {
+                cleanWs deleteDirs: true, notFailBuild: true
+                // cleanWs notFailBuild: true
+            }
+        }
+    }
+}
